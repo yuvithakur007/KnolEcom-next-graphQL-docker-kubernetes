ed733611878e39142fb133e867f676493cb19f8c
yuvithakur007 <01yuvisingh@gmail.com>
2024-04-01 01:39:20 +0530

    Id issur resolved

--- client/components/ProductDetails.jsx
+++ client/components/ProductDetails.jsx
@@ -1,51 +1,77 @@
 import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import styles from "../styles/details.module.css";
 import { useDarkMode } from "../context/DarkModeContext";
+import client from "../src/apollo-client.js";
+import { useMutation } from "@apollo/client";
+import { ADD_TO_CART } from "@/src/services/mutations";
+import { GET_SPECIFIED_PRODUCT } from "../src/services/quries.js";
 
-import { useMutation } from '@apollo/client';
-import { ADD_TO_CART } from '@/src/services/mutations';
+const ProductDetails = () => {
+  const router = useRouter();
+  const { id } = router.query;
 
-
-const ProductDetails = ({ product, productId }) => {
+  const [product, setProduct] = useState(null);
   const { state } = useDarkMode();
-
   const [addToCart] = useMutation(ADD_TO_CART);
 
-  const handleClick = async (e) => {
-    console.log("Product ID:", productId);
 
-    e.preventDefault();
-    const email = localStorage.getItem("email"); 
-  
-    if (localStorage.getItem('email')) {
-      try {
-        const { data } = await addToCart({
-          variables: {
-            productId,
-            email            
-          },
-        });
+  useEffect(() => {
+    console.log("Product ID:", id);
+    if (id) {
+      fetchProduct(id);
+    }
+  }, [id]);
 
-        const mssg = data.addToCart; 
-        if (mssg === "Added successfully!") {
-          alert(mssg);
-        } else {
-          throw new Error('Failed to add item to cart');
-        }
-      } catch (error) {
-        console.error(error);
-        alert('An error occurred while adding item to cart');
-      }
-    } else {
-      if (window.confirm("Please Login to add to cart")) {
-        window.location.href = '/login';
-      }
+  const fetchProduct = async (id) => {
+    const productId = id;
+    try {
+      const { data } = await client.query({
+        query: GET_SPECIFIED_PRODUCT,
+        variables: { productId },
+      });
+
+      const product = data.getProductById;
+      setProduct(product)
+    } catch (error) {
+      console.error("Error fetching specified product:", error);
+      return { props: { product: null } };
     }
   };
 
+  const handleClick = async (e) => {
+    console.log("Product ID:", id);
+
+    e.preventDefault();
+    const email = localStorage.getItem("email");
+
+    if (localStorage.getItem("email")) {
+      try {
+        const { data } = await addToCart({
+          variables: {
+            productId:id,
+            email,
+          },
+        });
+
+        const mssg = data.addToCart;
+        if (mssg === "Added successfully!") {
+          alert(mssg);
+        } else {
+          throw new Error("Failed to add item to cart");
+        }
+      } catch (error) {
+        console.error(error);
+        alert("An error occurred while adding item to cart");
+      }
+    } else {
+      if (window.confirm("Please Login to add to cart")) {
+        window.location.href = "/login";
+      }
+    }
+  };
   return (
     <div className={state.darkMode ? styles.darkMode : styles.lightMode}>
-  
       <div className={styles.productDetailsContainer}>
         {product && (
           <div className={styles.productDetails}>
@@ -54,12 +80,18 @@ const ProductDetails = ({ product, productId }) => {
             </div>
             <div className={styles.productInfo}>
               <h2 className={styles.productDetailsName}>{product.name}</h2>
-              <p className={styles.productDetailsDescription}>{product.description}</p>
-              <p className={styles.productDetailsCategory}>Category: {product.category}</p>
+              <p className={styles.productDetailsDescription}>
+                {product.description}
+              </p>
+              <p className={styles.productDetailsCategory}>
+                Category: {product.category}
+              </p>
               <div className={styles.detailsRating}>
                 <span>Rating: ★{product.avgRating}</span>
               </div>
-              <p className={styles.productDetailsPrice}>Price: ${product.price}</p>
+              <p className={styles.productDetailsPrice}>
+                Price: ${product.price}
+              </p>
               <div className={styles.productDetailsActions}>
                 <button className={styles.addCart} onClick={handleClick}>
                   Add to cart
@@ -70,9 +102,82 @@ const ProductDetails = ({ product, productId }) => {
         )}
       </div>
     </div>
-  );  
+  );
 };
-
-
-
 export default ProductDetails;
+
+// import React, { useState, useEffect } from "react";
+// import styles from "../styles/details.module.css";
+// import { useDarkMode } from "../context/DarkModeContext";
+
+// import { useMutation } from '@apollo/client';
+// import { ADD_TO_CART } from '@/src/services/mutations';
+
+// const ProductDetails = ({ product, productId }) => {
+//   const { state } = useDarkMode();
+
+//   const [addToCart] = useMutation(ADD_TO_CART);
+
+//   const handleClick = async (e) => {
+//     console.log("Product ID:", productId);
+
+//     e.preventDefault();
+//     const email = localStorage.getItem("email");
+
+//     if (localStorage.getItem('email')) {
+//       try {
+//         const { data } = await addToCart({
+//           variables: {
+//             productId,
+//             email
+//           },
+//         });
+
+//         const mssg = data.addToCart;
+//         if (mssg === "Added successfully!") {
+//           alert(mssg);
+//         } else {
+//           throw new Error('Failed to add item to cart');
+//         }
+//       } catch (error) {
+//         console.error(error);
+//         alert('An error occurred while adding item to cart');
+//       }
+//     } else {
+//       if (window.confirm("Please Login to add to cart")) {
+//         window.location.href = '/login';
+//       }
+//     }
+//   };
+
+//   return (
+//     <div className={state.darkMode ? styles.darkMode : styles.lightMode}>
+
+//       <div className={styles.productDetailsContainer}>
+//         {product && (
+//           <div className={styles.productDetails}>
+//             <div className={styles.productDetailsImage}>
+//               <img src={product.image} alt={product.name} />
+//             </div>
+//             <div className={styles.productInfo}>
+//               <h2 className={styles.productDetailsName}>{product.name}</h2>
+//               <p className={styles.productDetailsDescription}>{product.description}</p>
+//               <p className={styles.productDetailsCategory}>Category: {product.category}</p>
+//               <div className={styles.detailsRating}>
+//                 <span>Rating: ★{product.avgRating}</span>
+//               </div>
+//               <p className={styles.productDetailsPrice}>Price: ${product.price}</p>
+//               <div className={styles.productDetailsActions}>
+//                 <button className={styles.addCart} onClick={handleClick}>
+//                   Add to cart
+//                 </button>
+//               </div>
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ProductDetails;
--- client/pages/product/[id].js
+++ client/pages/product/[id].js
@@ -1,46 +1,52 @@
-import ProductDetails from '../../components/ProductDetails.jsx';
-import client from "../../src/apollo-client.js";
-import { GET_ALL_PRODUCT_IDS, GET_SPECIFIED_PRODUCT } from "../../src/services/quries.js";
+import ProductDetails from '@/components/ProductDetails';
 
-export default function ProductDetailsPage({ product, productId }) {
-    return <ProductDetails product={product} productId={productId} />;
+export default function ProductDetailsPage(){
+    return <ProductDetails />
 }
 
+// import ProductDetails from '../../components/ProductDetails.jsx';
+// import client from "../../src/apollo-client.js";
+// import { GET_ALL_PRODUCT_IDS, GET_SPECIFIED_PRODUCT } from "../../src/services/quries.js";
 
-export async function getStaticPaths() {
-  try {
-      const { data } = await client.query({
-          query: GET_ALL_PRODUCT_IDS,
-      });
+// export default function ProductDetailsPage({ product, productId }) {
+//     return <ProductDetails product={product} productId={productId} />;
+// }
 
-      const productIds = data.getAllProductIds;
 
-      const paths = productIds.map((productId) => ({
-          params: { Id: productId.toString() } 
-      }));
-      return { paths, fallback: false };
-  } catch (error) {
-      console.error("Error fetching product IDs:", error);
-      return { paths: [], fallback: false }; 
-  }
-}
+// export async function getStaticPaths() {
+//   try {
+//       const { data } = await client.query({
+//           query: GET_ALL_PRODUCT_IDS,
+//       });
 
-export async function getStaticProps({ params }) {
-  const productId = params.Id;
-  try {
-      const { data } = await client.query({
-          query: GET_SPECIFIED_PRODUCT,
-          variables: { productId }
-      });
+//       const productIds = data.getAllProductIds;
 
-      const product = data.getProductById;
-     return { 
-        props: { 
-          product,
-          productId 
-        } };
-  } catch (error) {
-      console.error("Error fetching specified product:", error);
-      return { props: { product: null } }; 
-  }
-}
+//       const paths = productIds.map((productId) => ({
+//           params: { Id: productId.toString() } 
+//       }));
+//       return { paths, fallback: false };
+//   } catch (error) {
+//       console.error("Error fetching product IDs:", error);
+//       return { paths: [], fallback: false }; 
+//   }
+// }
+
+// export async function getStaticProps({ params }) {
+//   const productId = params.Id;
+//   try {
+//       const { data } = await client.query({
+//           query: GET_SPECIFIED_PRODUCT,
+//           variables: { productId }
+//       });
+
+//       const product = data.getProductById;
+//      return { 
+//         props: { 
+//           product,
+//           productId 
+//         } };
+//   } catch (error) {
+//       console.error("Error fetching specified product:", error);
+//       return { props: { product: null } }; 
+//   }
+// }
