bf2b28d9b09d972b7f90907fe5ca34c608b02b75
yuvithakur007 <01yuvisingh@gmail.com>
2024-04-01 00:15:52 +0530

    appoloclient

--- client/src/apollo-client.js
+++ client/src/apollo-client.js
@@ -3,17 +3,6 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const client = new ApolloClient({
   uri: "https://eco-graphql.onrender.com/",
   cache: new InMemoryCache(),
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: 'no-cache', 
-    },
-    query: {
-      fetchPolicy: 'no-cache',
-    },
-    mutate: {
-      fetchPolicy: 'no-cache', 
-    },
-  },
 });
 
 export default client;
