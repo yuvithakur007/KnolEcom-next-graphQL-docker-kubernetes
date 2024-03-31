4183e8c0543259516bfa284567a42c42481b89b9
yuvithakur007 <01yuvisingh@gmail.com>
2024-04-01 01:01:57 +0530

    eront deploy

--- client/src/apollo-client.js
+++ client/src/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: "https://eco-graphql.onrender.com/",
+  uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
 });
 export default client;
