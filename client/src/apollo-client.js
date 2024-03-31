4a28a3c11f16612230b5ffe0985d79255ff142be
yuvithakur007 <01yuvisingh@gmail.com>
2024-03-31 23:36:26 +0530

    added vercel.json file

--- client/src/apollo-client.js
+++ client/src/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache} from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://eco-graphql.onrender.com/',
   cache: new InMemoryCache(),
 });
 
