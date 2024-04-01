69bb7d81c06e4bba73eacc42a4021a2a4bbf1e8f
yuvithakur007 <01yuvisingh@gmail.com>
2024-04-01 11:44:44 +0530

    edit vercel.json

--- client/src/apollo-client.js
+++ client/src/apollo-client.js
@@ -3,5 +3,16 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const client = new ApolloClient({
   uri: "https://eco-graphql.onrender.com/",
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'no-cache',
+    },
+    query: {
+      fetchPolicy: 'no-cache',
+    },
+    mutate: {
+      fetchPolicy: 'no-cache',
+    },
+  }
 });
 export default client;
